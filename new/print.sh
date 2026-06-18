0d1ebc8ace0c26c82f660f738b864c6989f63dc1
issallina <isalina1318@gmail.com>
2026-06-18 01:44:57 +0000

    new bash scripting files

---
+++ new/print.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Hello my name is Alina
+echo Alina is currently stydying linux
+echo Alina loves Devops
+echo 26*30*12
---
+++ new/variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=Alina
+
+echo Hello $name
+echo $name is curremtly studying Linux
+echo $name loves DevOps
+
+
